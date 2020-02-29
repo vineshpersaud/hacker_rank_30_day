ff22050bdb2d4bbff39dbaaa934082dd67cfd48b
github username <vineshpersaud@gmail.com>
2020-02-29 12:51:52 -0500

    sloved

---
+++ 30-conditional-statements.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+
+if  (N%2) != 0:
+    print ("Weird")
+elif ((N>=2) and (N<=5)) or N>20:
+    print ("Not Weird")
+else :
+    print("Weird")
