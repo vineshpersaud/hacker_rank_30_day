c58d11c529294feb236b0d485193f56233de10e4
github username <vineshpersaud@gmail.com>
2020-03-17 10:36:11 -0400

    sloved

---
+++ 30_abstract_classes.py
@@ -0,0 +1,10 @@
+class MyBook:
+    def __init__(self,title,author,price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display(self):
+        print('Title:', title)
+        print('Author:', author)
+        print('Price:', price)
